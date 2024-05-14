290884f3376047fc00944b724e8c50f2748e3801
sojer <seb.maz1996@gmail.com>
2024-05-14 10:40:41 +0200

    Configure project and routing

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { Router, Link, Route } from "svelte-routing";
+  import Auth from "./lib/Auth.svelte";
+  import Todo from "./lib/Todo.svelte";
+
+  export let url: string = "";
+</script>
+
+<Router {url}>
+  <div>
+    <Route path="/" component={Auth} />
+    <Route path="/todo" component={Todo} />
+  </div>
+</Router>
---
+++ src/lib/Auth.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { navigate } from "svelte-routing";
+
+  const onSubmit = () => {
+    navigate("/todo", { replace: true });
+  };
+</script>
+
+<div>
+  Login page
+  <button on:click={onSubmit}> Sign in </button>
+</div>
+
+<style>
+</style>

5c18333662b95602e5cb6752837ef6c3739d0371
sojer <seb.maz1996@gmail.com>
2024-05-20 15:59:21 +0200

    Refactor GoogleAuth component

--- src/routes/actions/GoogleAuth.svelte
+++ src/routes/actions/GoogleAuth.svelte
@@ -1,8 +1,10 @@
-<script>
+<script lang="ts">
   import { onMount } from "svelte";
   import { G_CLIENT_ID } from "../../util/global";
 
-  const handleCredentialResponse = (response) => {
+  let googleButton: HTMLDivElement;
+
+  const handleCredentialResponse = (response: any) => {
     console.log("Encoded JWT ID token: " + response.credential);
     // Handle the ID token here
   };
@@ -13,7 +15,7 @@
       callback: handleCredentialResponse,
     });
 
-    google.accounts.id.renderButton(document.getElementById("g_id_signin"), {
+    google.accounts.id.renderButton(googleButton, {
       theme: "outline",
       size: "large",
     });
@@ -27,4 +29,4 @@
   data-client_id={G_CLIENT_ID}
   data-callback="handleCredentialResponse"
 ></div>
-<div id="g_id_signin"></div>
+<div id="g_id_signin" bind:this={googleButton}></div>
